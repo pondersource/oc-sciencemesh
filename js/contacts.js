08b3c3a460c4c334c4cc819a48b69134198b580b
parham R <parhamin2010@gmail.com>
2023-01-11 21:04:13 +0330

    fix https://github.com/pondersource/oc-sciencemesh/issues/36

--- js/contacts.js
+++ js/contacts.js
@@ -8,51 +8,50 @@ $.ajax({
 }).done(function (response) {
     if(response === '' || response === false) {
         var element = document.getElementById("test_error");
-        element.innerHTML= 'No connection with reva';
+        element.innerHTML= 'No Sciencemesh Connection';
         $('#test_error').show(); 
     } else {
-    let token = JSON.parse(response);
-    for(tokenData in token) {
-        if(token.hasOwnProperty(tokenData)) {
-            console.log(tokenData);
-            if(tokenData === 'accepted_users') {
-                let accepted_users = token.accepted_users
-                    for(accept in accepted_users) {
-                        const displayName = accepted_users[accept].display_name;
-                        const username = accepted_users[accept].id.opaque_id;
-                        const idp = accepted_users[accept].id.idp;
-                        const provider = new URL(idp).host;
-                        const result = `
-                                <div href="#" class="app-content-list-item profile-item">
-                                    <div class="app-content-list-item-icon" style="">
-                                        <img src="https://cdn-icons-png.flaticon.com/512/16/16363.png">
-                                    </div>
-                                    <div class="app-content-list-item-line-one" id="show_result" >
-                                        <p class="displayname">${displayName}</p><p class="username-provider">${username}@${provider}</p>
-                                    </div>  
-                                </div>`;                  
-                        var element = document.getElementById("test_error");
-                        element.innerHTML = result;
-                    }
+        let token = JSON.parse(response);
+        for(tokenData in token) {
+            if(token.hasOwnProperty(tokenData)) {
+                if(tokenData === 'accepted_users') {
+                    let accepted_users = token.accepted_users
+                        for(accept in accepted_users) {
+                            const displayName = accepted_users[accept].display_name;
+                            const username = accepted_users[accept].id.opaque_id;
+                            const idp = accepted_users[accept].id.idp;
+                            const provider = new URL(idp).host;
+                            const result = `
+                                    <div href="#" class="app-content-list-item profile-item">
+                                        <div class="app-content-list-item-icon profile-item-img" style="">
+                                            <img src="https://cdn-icons-png.flaticon.com/512/16/16363.png">
+                                        </div>
+                                        <div class="app-content-list-item-line-one profile-item-username" id="show_result" >
+                                            <p class="displayname">${displayName}</p><p class="username-provider">${username}@${provider}</p>
+                                        </div>  
+                                    </div>`;                  
+                            var element = document.getElementById("test_error");
+                            element.innerHTML = result;
+                        // }
 
-                $('#test_error').show();
-            }else{
-                const result = `
-                        <div href="#" class="app-content-list-item profile-item" >
-                            <div class="app-content-list-item-icon" style="">
-                            </div> 
-                            <div class="app-content-list-item-line-one" id="show_result" >
-                                <p class="username-provider">There're no contacts!</p>
-                            </div>  
-                        </div>`;                  
-                var element = document.getElementById("test");
-                element.innerHTML = result;
-                $('#test_error').show();
+                    $('#test_error').show();
+                }else{
+                    const result = `
+                            <div href="#" class="app-content-list-item profile-item" >
+                                <div class="app-content-list-item-icon" style="">
+                                </div> 
+                                <div class="app-content-list-item-line-one" id="show_result" >
+                                    <p class="username-provider">There're no contacts!</p>
+                                </div>  
+                            </div>`;                  
+                    var element = document.getElementById("test");
+                    element.innerHTML = result;
+                    $('#test_error').show();
 
-            }
-        } 
+                }
+            } 
+        }
     }
-}
 }).fail(function (response, code) {
     console.log(response)
     //alert('The token is invalid')
@@ -76,7 +75,7 @@ document.getElementById('elem').onclick = function () {
       
        if(response === '' || response === false) {
             var element = document.getElementById("test_error");
-            element.innerHTML= 'No connection with reva';
+            element.innerHTML= 'No Sciencemesh Connection';
         } else {
             let result = JSON.parse(response);
             if(result.hasOwnProperty('message')) {
