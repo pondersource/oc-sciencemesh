ff95603d0c1b2f29a14d5d7fa78193227265dbc8
Michiel de Jong <michiel@unhosted.org>
2023-01-16 13:34:42 +0000

    paging happens in the calling function

--- lib/Plugins/ScienceMeshSearchPlugin.php
+++ lib/Plugins/ScienceMeshSearchPlugin.php
@@ -27,17 +27,13 @@ class ScienceMeshSearchPlugin {
 		$this->revaHttpClient = new RevaHttpClient($this->config);
 	}
 
-	public function search($search, $limit, $offset) {
+	public function search($search) {
 		$result = json_decode($this->revaHttpClient->findAcceptedUsers($this->userId), true);
 		if (!isset($result['accepted_users'])) {
 			return [];
 		}
 		$users = $result['accepted_users'];
         error_log("Found " . count($users) . " users");
-		// $users = array_filter($users, function ($user) use ($search) {
-		// 	return (stripos($user['display_name'], $search) !== false);
-		// });
-		$users = array_slice($users, $offset, $limit);
 
 		$result = [];
 		foreach ($users as $user) {
@@ -49,16 +45,6 @@ class ScienceMeshSearchPlugin {
 					'shareType' => Share::SHARE_TYPE_REMOTE,
 					'shareWith' => $user['id']['opaque_id'] ."@". $user['id']['idp'],
 				],
-			
-				// "label" => "Label",
-				// "uuid" => $user['id']['opaque_id'],
-				// "name" => $user['display_name'] ."@". $domain, // FIXME: should this be just the part before the @ sign?
-				// "type" => "ScienceMesh",
-				// "value" => [
-				// 	"shareType" =>  \OCP\Share::SHARE_TYPE_REMOTE,
-				// 	"shareWith" => $user['id']['opaque_id'] ."@". $domain, // FIXME: should this be just the part before the @ sign?
-				// 	"server" => $user['id']['idp']
-				// ]
 			];
 		}
 		error_log("returning result:");
