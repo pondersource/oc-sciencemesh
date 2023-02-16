e312fd37928d9c39460e313cae88db5f0ac67208
parham R <parhamin2010@gmail.com>
2023-02-16 19:19:36 +0330

    fix some style on setting page

--- templates/settings/admin.php
+++ templates/settings/admin.php
@@ -1,3 +1,4 @@
+
 <?php 
 /**
  * controller <controller/SettingsController.php>
@@ -10,6 +11,7 @@
  * author <parhamin2010@gmail.com>
  * */
 
+
 script('sciencemesh', 'settings');
 style('sciencemesh', 'style');
 ?>
@@ -17,13 +19,14 @@ style('sciencemesh', 'style');
     <div class="viewcontainer section-sciencemesh">
         <div class="sciencemesh-settings-row">
             <label for="sciencemesh_iop_url" name="sciencemesh_iop_url">IOP URL</label>
-            <input type="text" name="sciencemesh_iop_url" id="sciencemesh_iop_url" value="<?php echo $this->vars['sciencemeshIopUrl']; ?>">
-            <button type="button" id="check_connection_sciencemesh_iop_url"  >Connection test</button>
+            <input type="text" style="width: 65%;" name="sciencemesh_iop_url" id="sciencemesh_iop_url" value="<?php echo $this->vars['sciencemeshIopUrl']; ?>">
+            <button type="button" id="check_connection_sciencemesh_iop_url">Connection test</button>
         </div>
         <div class="sciencemesh-settings-row">
             <label for="sciencemesh_shared_secret" name="sciencemesh_shared_secret">Shared Secret</label>
-            <input type="text" name="sciencemesh_shared_secret" id="sciencemesh_shared_secret" value="<?php echo $this->vars['sciencemeshRevaSharedSecret']; ?>">
+            <input type="text" style="width: 77%;"id="sciencemesh_shared_secret" value="<?php echo $this->vars['sciencemeshRevaSharedSecret']; ?>">
         </div>
+        <hr style="opacity: 0.1;">
         <input type="button" name="sciencemesh_setting_submit_btn" id="sciencemesh_setting_submit_btn" value="Save settings">
         </div>
     </div>
